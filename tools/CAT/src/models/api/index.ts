cbbe42bc581f4193f2d19cb664141df875382b25
Maksym Marianchuk <maksym.marianchuk@skykick.com>
2023-10-23 10:59:12 +0300

    Models Generator

---
+++ tools/CAT/src/models/api/index.ts
@@ -0,0 +1,36 @@
+// DO NOT EDIT BY HANDS
+
+// ..\..\services\College.Domain\DTOs\CategoryDto.cs
+export interface CategoryDto {
+    id: string;
+    url: string;
+    title: string;
+    subCategories: SubCategoryDto[];
+}
+
+// ..\..\services\College.Domain\DTOs\NewsDto.cs
+export interface NewsDto {
+    id: string;
+    title: string;
+    mainImage: string;
+    description: string;
+    content: string;
+    pinned?: boolean;
+    date: string;
+}
+
+// ..\..\services\College.Domain\DTOs\PageDto.cs
+export interface PageDto {
+    id: string;
+    url: string;
+    title: string;
+    content: string;
+}
+
+// ..\..\services\College.Domain\DTOs\SubCategoryDto.cs
+export interface SubCategoryDto {
+    id: string;
+    url: string;
+    title: string;
+    pages: PageDto[];
+}
