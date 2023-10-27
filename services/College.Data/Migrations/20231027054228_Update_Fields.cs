2124b0a63405291fb3b2ffb8f479ad2f9ab2e743
Maks <lion20914king@gmail.com>
2023-10-27 08:44:13 +0300

    New Foriegn keys

---
+++ services/College.Data/Migrations/20231027054228_Update_Fields.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace College.Data.Migrations
+{
+    public partial class Update_Fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_SubCategories_SubCategoryId",
+                table: "Pages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CategoryId",
+                table: "SubCategories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SubCategoryId",
+                table: "Pages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_SubCategories_SubCategoryId",
+                table: "Pages",
+                column: "SubCategoryId",
+                principalTable: "SubCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pages_SubCategories_SubCategoryId",
+                table: "Pages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CategoryId",
+                table: "SubCategories",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SubCategoryId",
+                table: "Pages",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pages_SubCategories_SubCategoryId",
+                table: "Pages",
+                column: "SubCategoryId",
+                principalTable: "SubCategories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategories_Categories_CategoryId",
+                table: "SubCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+    }
+}
