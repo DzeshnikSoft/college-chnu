ec6890884de32e46a71b7c6a1be27e9be6b10cea
Vova-Romaniuk <romanyuk1504@gmail.com>
2023-10-31 14:04:21 +0200

    resolva conflict

--- tools/CAT/src/components/Button/index.tsx
+++ tools/CAT/src/components/Button/index.tsx
@@ -3,15 +3,9 @@ import { BasePropsButton } from '@/models/ui';
 export default function Button({ children, className = '', onClick }: BasePropsButton) {
 	return (
 		<button
-<<<<<<< HEAD
 			type='submit'
 			className={`text-xl rounded-sm bg-activeItems cursor-pointer p-2 ${className}`}
 			onClick={onClick}>
-=======
-			className={`text-xl rounded-md bg-activeItems cursor-pointer p-2 ${className}`}
-			onClick={onClick}
-		>
->>>>>>> development
 			{children}
 		</button>
 	);
