b67075e58feb57b90ec97501c873474021b3fd64
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2023-12-14 15:10:02 +0200

    footer changes

---
+++ web/src/components/SocialMediaIcon/index.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+function SocialMediaIcon({ name }) {
+	const [iconData, setIconData] = useState({
+		className: '',
+		url: '',
+	});
+
+	useEffect(() => {
+		selectIcons(name);
+	}, []);
+
+	const selectIcons = (name) => {
+		switch (name) {
+			case 'instagram':
+				setIconData({
+					className: 'fab fa-instagram',
+					url: 'https://www.instagram.com',
+				});
+				return;
+			case 'facebook':
+				setIconData({
+					className: 'fab fa-facebook-f',
+					url: 'https://www.facebook.com',
+				});
+				return;
+			case 'tiktok':
+				setIconData({
+					className: 'fab fa-tiktok',
+					url: 'https://www.tiktok.com',
+				});
+				return;
+			default:
+				return '';
+		}
+	};
+	return (
+		<a href={iconData.url} target='_blank' rel='noreferrer'>
+			<i
+				className={`text-accentTextColor cursor-pointer text-2xl duration-100 hover:text-mainTextColor hover:scale-105 ${iconData.className}`}></i>
+		</a>
+	);
+}
+
+export default SocialMediaIcon;
