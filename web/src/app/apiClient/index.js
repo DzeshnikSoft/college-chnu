e04812df6a5adf7a79864810eb7838ffb404cb7e
Maksym Marianchuk <maksym.marianchuk@skykick.com>
2024-01-13 16:33:44 +0200

    Remove api key

--- web/src/app/apiClient/index.js
+++ web/src/app/apiClient/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const { REACT_APP_API_URL } = process.env;
+const { REACT_APP_API_URL, REACT_APP_COLLEGE_API_KEY } = process.env;
 
 export const apiClient = axios.create({
     baseURL: REACT_APP_API_URL,
     responseType: 'json',
     headers: {
-        'college-authorization': 'b0c7793e7f3e4ca8973dcef8f1b5f096',
+        'college-authorization': REACT_APP_COLLEGE_API_KEY,
     },
 });
