64519b423ba30892b488dc06ee5b4b870f9f4da8
MaksMaryanchuk <64914675+maryanchuk04@users.noreply.github.com>
2024-01-27 21:50:44 +0200

    CI build (#66)
    * CI build
    
    * One more try
    
    * One more try 2
    
    * One more try 3:
    
    * Try again 4
    
    * Fix UI build
    
    * Revert Footer
    
    * Remove useEffect
    
    ---------
    
    Co-authored-by: Maksym Marianchuk <maksym.marianchuk@skykick.com>

--- web/src/components/TextOverImage/index.jsx
+++ web/src/components/TextOverImage/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function TextOverImage({ children, src }) {
+export default function TextOverImage({ children, src, alt = 'background-image' }) {
 	return (
 		<div className='h-full w-full relative'>
 			<img
 				src={src}
+				alt={alt}
 				className='w-full h-full object-cover z-20'
 				loading='lazy'
 			/>
