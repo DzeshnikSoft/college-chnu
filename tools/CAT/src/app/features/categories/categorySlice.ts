dfeca784fe585ee04f18041ef63fb79b74a2ba9b
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-02-28 23:18:24 +0200

    ui/ux part move navmenu items is done

--- tools/CAT/src/app/features/categories/categorySlice.ts
+++ tools/CAT/src/app/features/categories/categorySlice.ts
@@ -22,13 +22,63 @@ const initialState: CategoryInitialState = {
 const categorySlice = createSlice({
 	name: 'category',
 	initialState,
-	reducers: {},
+	reducers: {
+		moveCategory: (state, action) => {
+			const { currentIndex, newIndex } = action.payload;
+			const movedItem = state.categories.splice(currentIndex, 1)[0];
+			state.categories.splice(newIndex, 0, movedItem);
+		},
+		moveSubCategory: (state, action) => {
+			const { currentIndex, newIndex, categoryId } = action.payload;
+
+			const category = state.categories.find(
+				(cat) => cat.id === categoryId
+			);
+
+			if (category) {
+				const subCategories = [...category.subCategories];
+
+				const movedSubCategory = subCategories.splice(
+					currentIndex,
+					1
+				)[0];
+
+				subCategories.splice(newIndex, 0, movedSubCategory);
+				category.subCategories = subCategories;
+			}
+		},
+
+		movePage: (state, action) => {
+			const { currentIndex, newIndex, subCategoryId } = action.payload;
+
+			const category = state.categories.find((cat) =>
+				cat.subCategories.some((subCat) => subCat.id === subCategoryId)
+			);
+
+			if (category) {
+				const subCategory = category.subCategories.find(
+					(subCat) => subCat.id === subCategoryId
+				);
+
+				if (subCategory) {
+					const pages = [...subCategory.pages];
+					const movedPage = pages.splice(currentIndex, 1)[0];
+
+					pages.splice(newIndex, 0, movedPage);
+					subCategory.pages = pages;
+				}
+			}
+		},
+	},
 	extraReducers: (builder) => {
 		extraReducersConfigCategories(builder);
 		extraReducersConfigSubCategories(builder);
 		extraReducersConfigPages(builder);
 	},
 });
+
+export const { moveCategory, moveSubCategory, movePage } =
+	categorySlice.actions;
 
 export const getСategoryDataSelector = (state: RootState) =>
 	state.category.categories;
