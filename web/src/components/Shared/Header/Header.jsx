b0d3cf663b1c2f35b7aa67b404d35bc7550e83b5
Vova Romanyuck <vovaromanyuck@MacBook-Air-Vova.local>
2024-06-16 00:18:59 +0300

    refactor-code 8.0

--- web/src/components/Shared/Header/Header.jsx
+++ web/src/components/Shared/Header/Header.jsx
@@ -57,7 +57,7 @@ function Header() {
 								<NavLink
 									to='/'
 									className='h-full flex items-center'>
-									Коледж
+									Головна
 								</NavLink>
 								{/* <SubMenu
 									subMenu={[
